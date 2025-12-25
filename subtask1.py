0182562af6634d53a25875c6c3e6985bebb32df7
Daniel Dimitrov <s2510769042@fhooe.at>
2025-12-25 17:09:12 +0100

    Add subtask1 and subtask2 scripts

---
+++ subtask1.py
@@ -0,0 +1,9 @@
+def task_73(n):
+    k = 0
+    q = 0
+    while((k+1)*(k+1)<=n):
+        k = k+1
+        q = k*k
+        
+    print(q)
+task_73(31)  
---
+++ subtask2.py
@@ -0,0 +1,23 @@
+def task_74():
+    n = 0
+    s = 0
+    m = float('inf')
+    
+    print("Enter numbers one by one. Enter -1 to finish:")
+    
+    while (x := float(input())) != -1:
+        n += 1
+        s += x
+        if x < m:
+            m = x
+            
+    if n == 0:
+        m = -1
+        a = -1
+    else:
+        a = s / n
+        
+    print(f"n={n}, s={s}, m={m}, a={a}")
+    
+    
+task_74()
